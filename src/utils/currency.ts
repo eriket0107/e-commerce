15c079089a5cd1f5e1d282b3a97b0d771446d0c9
Erik <ol.erik0107@gmail.com>
2023-12-13 10:29:13 -0300

    feat: creating api response type product and util currency func

---
+++ src/utils/currency.ts
@@ -0,0 +1,7 @@
+export const PriceCurrencyReal = (price: number) => {
+  return new Intl.NumberFormat('pt-BR', {
+    style: 'currency',
+    currency: 'BRL',
+    minimumFractionDigits: 0,
+  }).format(price)
+}
---
+++ src/utils/index.ts
@@ -0,0 +1,1 @@
+export { PriceCurrencyReal } from './currency'
